e4c98c7ef3b05e69d20cde0b59b3662ee039d856
Nezarik IntMax <38297663+Nezarik-Intmax@users.noreply.github.com>
2018-12-02 14:05:41 +0500

    Add files via upload

---
+++ Nezarik-Intmax.github.io-master/SeoComany/js/scroll.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".trigger").on("click",function(t){t.preventDefault(),console.log("asdf");var a=$(this).attr("href").replace("#",""),e={};$("[data-target]").each(function(){var t=$(this);trigger=t.data("target"),trigger==a&&(e=t)});var r=e.offset().top;$("html, body").animate({scrollTop:r},500)})}); 
