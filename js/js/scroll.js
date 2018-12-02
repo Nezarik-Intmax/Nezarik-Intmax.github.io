bb4bbc8bc268ea569611af651fed553389abb8fc
Nezarik IntMax <38297663+Nezarik-Intmax@users.noreply.github.com>
2018-12-02 11:38:47 +0500

    Delete scroll.js

--- js/js/scroll.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".trigger").on("click",function(t){t.preventDefault(),console.log("asdf");var a=$(this).attr("href").replace("#",""),e={};$("[data-target]").each(function(){var t=$(this);trigger=t.data("target"),trigger==a&&(e=t)});var r=e.offset().top;$("html, body").animate({scrollTop:r},500)})});
